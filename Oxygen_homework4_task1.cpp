23a5609cbae58402d35088b6fd997b9a801663e2
Tyler <tylersandoval@mail.weber.edu>
2017-10-10 13:17:22 -0600

    Added template for the assignment

--- Oxygen_homework4_task1.cpp
+++ Oxygen_homework4_task1.cpp
@@ -2,8 +2,39 @@
 #include <string>
 using namespace std;
 
+// Constants, Structs, Classes
+const int MAX = 3;
+
+class HouseInfo
+{
+private:
+    int streetNum;
+    float price;
+    string streetName;
+public:
+    void setInfo()
+    {
+        cout << "Enter street number: " << endl;
+        cin >> streetNum;
+        cout << "Enter house price: " << endl;
+        cin >> price;
+        cout << "Enter street name: " << endl;
+        getline(cin, streetName);
+
+        cin.ignore(); // flush buffer for the string input
+    }
+};
+// Prototypes
+void ComparePrices(HouseInfo h[]);
+
+// Main program
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
     return 0;
+}
+// Function Definitions
+void ComparePrices(HouseInfo h[])
+{
+
 }
